cd70d81e04e2366c195e7fda48d3c80d4f436be0
Naufal Rayan Attallah <hanya1atta@gmail.com>
2026-02-24 13:46:33 +0700

    Commit pertemuan2

---
+++ IntroTest/src/Titik.java
@@ -0,0 +1,8 @@
+// Nama File : Titik.java
+// Deskripsi : Kelas untuk merepresentasikan titik dalam koordinat 2D
+// Pembuat  : Naufal Rayan Attallah
+// Tanggal   : 20 Februari 2024
+
+public class Titik {
+    
+}
---
+++ Pertemuan1/src/Titik.java
@@ -0,0 +1,8 @@
+// Nama File : Titik.java
+// Deskripsi : Kelas Titik untuk merepresentasikan titik dalam bidang 2D
+// Pembuat  : Naufal Rayan Attallah
+// Tanggal   : 27 Maret 2024
+
+public class Titik {
+    
+}
