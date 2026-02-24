56b91ea80ede54ba76224881de9b6360cd0f2add
Naufal Rayan Attallah <hanya1atta@gmail.com>
2026-02-24 14:58:26 +0700

    Titik Pertemuan2

---
+++ Pertemuan2/src/Mtitik.java
@@ -0,0 +1,27 @@
+//Nama : Mtitik.java
+//Deskripsi : berisi atribut dan method dalam class titik
+//Pembuat : Naufal Rayan Attallah
+//Tanggal : 24-02-2026
+
+public class Mtitik {
+    public static void main(String[] args) {
+        Titik T1 = new Titik();
+        T1.setAbsis(3);
+        T1.setOrdinat(4);
+        T1.printTitik();
+        T1.geser(3, 4);
+        T1.printTitik();
+
+        Titik T2 = T1;
+        T2.printTitik();
+        T1.setAbsis(10);
+        T1.setOrdinat(10);
+        T1.printTitik();
+
+        Titik T1 = new Titik(); //Membuat objek titik T1 (0,0)
+        Titik T2 = new Titik(3,5); //Membuat objek titik T2 (3,5)
+
+        System.out.println("Jumlah Objek Titik = " + Titik.getCounterTitik());
+        System.out.println("Jumlah Objek Titik = " + T2.getCounterTitik());
+    }
+}
---
+++ Pertemuan2/src/Titik.java
@@ -0,0 +1,103 @@
+//Nama File : Titik.java
+//Deskripsi : berisi atribut dan method dalam calss Titik
+//Pembuat   : Naufal Rayan Attallah
+//Tanggal   : 24-02-2026
+
+public class Titik {
+    //Atribut
+    double absis;
+    double ordinat;
+    static int counterTitik = 0;
+
+    //Method
+    //Konstruktor utk membuat titik (x,y)
+    Titik(double absis, double ordinat){
+        this.absis = absis;
+        this.ordinat = ordinat;
+        counterTitik++;
+    }
+
+    //Konstruktor untuk membuat titik (0,0)
+    Titik(){
+        this(0,0);
+    }
+
+    //mengembalikan nilai counterTitik
+    static int getCounterTitik(){
+        return counterTitik;
+    }
+
+    //Mengembalikan nilai absis dan ordinat
+    double getAbssis(){
+        return absis;
+    }
+
+    double getOrdinat(){
+        return ordinat;
+    }
+
+    //Mengeset absis titik dengan nilai baru x
+    void setAbsis(double x){
+        absis = x;
+    }
+
+    //mengeset ordinat titik dengan nilai baru y
+    void setOrdinat(double y){
+        ordinat = y;
+    }
+
+    //menggeser nilai absis dan ordinat titik masing-masing sejauh x dan y
+    void geser(double x, double y){
+        absis = absis + x;
+        ordinat = ordinat + x;
+    }
+
+    //mencetak koordinat titik
+    void printTitik(){
+        System.out.println("Titik (" + absis + "," + ordinat + ") ");
+    }
+
+    void printCounterTitik(){
+        System.out.println("Jumlah Objek Titik = " + Titik.counterTitik);
+    }
+
+    double jarak(Titik T){
+        double jarak = Math.sqrt(Math.pow((absis - T.absis), 2) + Math.pow((ordinat - T.ordinat), 2));
+        return jarak;
+    }
+
+    double kuadran(){
+        if (absis > 0 && ordinat > 0){
+            return 1;
+        } else if (absis < 0 && ordinat > 0){
+            return 2;
+        } else if (absis < 0 && ordinat < 0){
+            return 3;
+        } else if (absis > 0 && ordinat < 0){
+            return 4;
+        } else {
+            return 0; //titik berada pada sumbu x atau y
+        }
+    }
+    
+    void refleksiX(){
+        ordinat = -ordinat;
+    }
+
+    void refleksiY(){
+        absis = -absis;
+    }
+
+    double JarakPusat(){
+        double jarakPusat = Math.sqrt(Math.pow(absis, 2) + Math.pow(ordinat, 2));
+        return jarakPusat;
+    }
+
+    double getRefleksiX(){
+        return new Titik(absis, -ordinat);
+    }
+
+    double getRefleksiY(){
+        return new Titik(-absis, ordinat);
+    }
+}
